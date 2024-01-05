c13af204ed85e702218d50c074d3a6967011b538
Moon Ma <moon.ma@opensesame.com>
2024-01-05 10:19:44 -0500

    tested with a public commnunity

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
-# scraper
+*Please don't judge the engineering practice you see here ...*
+
+*Use this to make money and have fun!*
+
+# Manual
+
+- This scraper is specifically designed to scrape posts on [skool](https://www.skool.com/). You will need to create an account there and update the code with your credentials
+- This code doesn't include the preparation of those URLs. You can use [Bardeen.ai](https://www.bardeen.ai/) to do that for free without coding.
+- The sample URLs in `test_urls.csv` are posts in a [public community](https://www.skool.com/community) of skool. You may need to join that group first. It's free.
+
+# High Level Steps
+
+**(URLs →) Auth → Check URL → Expand → Read, Parse, Store**
+
+1. Load a list of urls from CSV (I used the free feature of Bardeen.ai to perpare this)
+2. Authentificate the access to the page you gonna scrape using Selenium
+3. Check if the url still exists, they may get deleted by the time of scraping
+4. Expand all the nested data on the page
+5. Read and parse the data into a structure easy to retrieve and digest
+
+
+# Technical Lessons
+
+> *Warning, this is boring ...*
+
+> - If you ask how engineers learn coding, this is how. ¯\\\_(ツ)_/¯
+> - Trial and error, until you are frustrated enough to ask for help ಠ_ಠ
+> - (even though humans tend to be very annoying because they never have a stable API with documentation! 😂)
+
+1. Selenium 
+    - It is the solution to solve the authentification issue. Initially, I tried using Python request directly and include the header and cookies, but that didn't work because of this AWSALBCORS thing. 
+    - BTW, I have a certification of AWS Solution Architect Associate, but, no, that didn't help. No certification can increase my patience.
+2. Page Status
+    - I encoutered this page displaying 404, so I was looking for 404 in the request status to handle it, but couldn't capture it, until I realized that that's just the UI, the actual code still returend 200. 
+    - I found it funny because it's like the page lied to you.
+3. XPath
+    - It is the easiest way to select an element
+    - Here is how to get it in Chrome
+4. chromedriver 
+    - It is the tool to interact with the elements on the page
+    - but still need request to check status code
+5. Nested Content
+    - there may be nested content on a page, we need to check and expand all of them if there are
+    - after the code clicks open a page, it may take a while to load, so set some sleep time as safety after `click()` so won’t throw error at you
+6. Compoud Class
+    - XPath is not convienient since you can only target a specific element on the page. If you want to find all elements that satisify certain criteria, you may try `find_elements_by_class_name()`
+    - Then the problem is that a class name may have multiple elements in it, which is called compound class name, then you need to use a css selector
+7. CSV reader is an iterator
+    - so you just use next() to skip the header
+8. CSV multi-line cell
+    - to my surprise, it’s actually pretty easy to write multiple lines in single cell in csv. the new line character `\n` is interpreted as expected.
+8. Edge Case
+    - just like in LeetCode, some post only have title, no content, some watch doesn't have number in it, this messes up data parsing. The page doesn't have placeholder for values expected but don't actually exist.
+    - you need large enough data to encounter those edge cases
+9. Test For POC 
+    - make sure start with small tasks and have tests in place, I scraped these 10k entires twice because I didn't realize those "nested data" issue the first time until I started eyeballing and reading the output data
+10. Money!
+    - pretty happy overall. This would have cost me $3,000 otherwise using tools like Bardeen. OK, this isn't technical, but it's the WHY! leadership is about the WHY! life is about the WHY! or is it...
+    - maybe we can just have fun..
---
+++ skool_posts_scraper.py
@@ -0,0 +1,118 @@
+import time, csv, random, requests, re, os
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+DRIVER_PATH = '/Users/moonma/Downloads/chromedriver-mac-x64/chromedriver'
+MIN_PAUSE_TIME = 80
+MAX_PAUSE_TIME = 250
+URL_FNAME = 'test_urls.csv'
+POST_FNAME = URL_FNAME[:-4] + "_output.csv"
+
+service = Service(executable_path=DRIVER_PATH)
+options = webdriver.ChromeOptions()
+driver = webdriver.Chrome(service=service, options=options)
+driver.get('https://www.skool.com/community/about')
+
+loginXPath = '//*[@id="__next"]/div/div/div[1]/div/div/div[3]/div/button[2]/span[1]/p'
+login = driver.find_element(By.XPATH, loginXPath)
+print(login.text)
+
+driver.find_element(By.XPATH, loginXPath).click()
+formXPath = '//*[@id="__next"]/div[2]/div[2]/div[2]/div/form/div'
+form = driver.find_element(By.XPATH, formXPath)
+print(form.text)
+
+emailXPath = '//*[@id="email"]/input'
+email = driver.find_element(By.XPATH, emailXPath).send_keys("moon30biology@gmail.com")
+
+passwordXPath = '//*[@id="password"]/input'
+password = driver.find_element(By.XPATH, passwordXPath).send_keys(".axf.ve8swKjey6")
+
+submitXPath = '//*[@id="__next"]/div[2]/div[2]/div[2]/div/form/button[2]/span'
+submit = driver.find_element(By.XPATH, submitXPath).click()
+
+time.sleep(5)
+
+with open(URL_FNAME, 'r', newline='') as infile, open(POST_FNAME, 'a', newline='') as outfile:
+    csvreader = csv.reader(infile, delimiter=',') 
+    next(csvreader, None)
+    csvwriter = csv.writer(outfile, delimiter=',')
+    csvwriter.writerow(["URL", "Title", "AuthorLevel", "AuthorName", "PublishTime", "Category", "WacthCount", \
+                        "ContentLinesCount", "Content", "CommentsCount", "AllComments"])
+    random.seed(a=33, version=2)
+    row_ct = 0
+    for row in csvreader:
+        title, url = row[0], row[1]
+        print(f"Row #{row_ct} started: {title}, url: {url}")
+        driver.get(url)
+
+        # make sure post isn't deleted by the time of scraping
+        try:
+            titleXPath = '//*[@id="__next"]/div/div/div[3]/div/div[1]/div/div/div[1]/div/div[1]/div/div[2]/div/div/span'
+            driver.find_element(By.XPATH, titleXPath).click()
+        except Exception:
+            url_status = requests.get(url).status_code
+            print("   ", url_status, url, "→ NOT WORKING!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+            continue
+
+        time_pause = random.randint(MIN_PAUSE_TIME, MAX_PAUSE_TIME)/100
+        time.sleep(time_pause)
+        row_ct += 1
+
+        # expand all "View x more reply", there may be nested cases
+        while True:
+            morereplies = driver.find_elements(By.CSS_SELECTOR, ".styled__ExpandRepliesLabel-sc-1lql1qn-8.cEAa-dK")
+            if morereplies == []: break
+            print(f"    'more replies' found: {len(morereplies)}")
+            for mr in morereplies: 
+                driver.execute_script("arguments[0].click();", mr)
+                time.sleep(1.5)
+
+        # expand all "See more", there shouldn't be any nested case
+        seemores =  driver.find_elements(By.CLASS_NAME, "jss62")
+        print(f"    'See more' found: {len(seemores)}")
+        for sm in seemores: 
+            try:
+                sm.click()
+                time.sleep(1.5)
+            except Exception:
+                os.system('say "See more click failed"')
+                print('-------------------------------------See more click failed:\n', Exception)
+        
+        # parsing post
+        postXPath = '//*[@id="__next"]/div/div/div[3]/div/div[1]/div/div/div[1]/div/div[1]/div'
+        post_elements = driver.find_element(By.XPATH, postXPath).text.splitlines()
+        author_level = post_elements[0]
+        author_name = post_elements[1]
+        splitIndex = post_elements[2].index("in")
+        publish_time = ''.join(post_elements[2][:splitIndex])
+        category = ''.join(post_elements[2][splitIndex+3:])
+        watch_ct = re.findall("\d+", post_elements[3])
+        if watch_ct: watch_ct = int(watch_ct[0]) # edge case where there is no number
+        content_lines_ct = len(post_elements) - 5 # each line in the content of the post is a single element in post_elements
+        print(f"    content_lines_ct: {content_lines_ct}")
+        post_content = ''
+        for i in range(5, len(post_elements)): # edge case where the post doesn't have text content
+            if i != len(post_elements) - 1:
+                post_content += post_elements[i] + "\n\n"
+            else:
+                post_content += post_elements[i]
+
+        # parsing comments
+        commentsXPath = '//*[@id="__next"]/div/div/div[3]/div/div[1]/div/div/div[1]/div/div[2]/div[2]/div[1]'
+        comments_elements = driver.find_element(By.XPATH, commentsXPath).text.splitlines()
+        comments_ct, all_comments, comment = 0, '', []
+        for i, el in enumerate(comments_elements): 
+            comment.append(el)
+            if el == "Reply":
+                if i != len(comments_elements) - 1:
+                    all_comments += ' '.join(comment) + "\n\n"
+                else:
+                    all_comments += ' '.join(comment)
+                comment = []
+                comments_ct += 1
+
+        csvwriter.writerow([url, title, author_level, author_name, publish_time, category, watch_ct, content_lines_ct, post_content, comments_ct, all_comments])
+        print('    completed')
+driver.quit()
